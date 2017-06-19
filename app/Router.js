aeceeda6c90cec71501e2052909037ff251d7138
shellfly <shell0fly@gmail.com>
2017-06-19 08:44:19 +0800

    add new files

---
+++ app/Router.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Scene, Router, ActionConst } from 'react-native-router-flux';
+import {
+  TabNavigator,
+} from 'react-navigation';
+import CookieManager from 'react-native-cookies';
+
+import AuthLogin from './screens/AuthLogin'
+import HomeStack from './screens/Home';
+import ReadScreen from './screens/Read';
+import ForumScreen from './screens/Forum';
+import ProfileScreen from './screens/Profile';
+
+const HOME_URL = "https://souka.io/";
+
+const App = TabNavigator({
+  Home: {
+    screen: HomeStack,
+  },
+  Read: {
+    screen: ReadScreen,
+  },
+  Forum: {
+    screen: ForumScreen,
+  },
+  Profile: {
+    screen: ProfileScreen,
+  }
+});
+
+class RouterComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: false,
+      loadedCookie: false
+    };
+  }
+
+  componentWillMount () {
+    CookieManager.get(HOME_URL, (err, cookie) => {
+      let isAuthenticated;
+      // If it differs, change `cookie.remember_me` to whatever the name for your persistent cookie is!!!
+      if (cookie && cookie.hasOwnProperty('sessionid')) {
+        isAuthenticated = true;
+      }
+      else {
+        isAuthenticated = false;
+      }
+
+      this.setState({
+        loggedIn: isAuthenticated,
+        loadedCookie: true
+      });
+    });
+  }
+
+  render(){
+    if (this.state.loadedCookie) {
+      return (
+        <Router>
+          <Scene key="login" component={AuthLogin} title="登录" initial={!this.state.loggedIn}/>
+          <Scene key="main" component={App} hideNavBar={true} initial={this.state.loggedIn}/>
+        </Router>
+        )
+    }
+    else {
+      return (
+        <View></View>
+      );
+    }
+  }
+}
+
+export default RouterComponent;
---
+++ app/screens/AuthLogin.js
@@ -0,0 +1,50 @@
+'use strict';
+
+import React,  { Component } from 'react';
+import { StyleSheet, View, WebView } from 'react-native';
+import { Actions, ActionConst } from 'react-native-router-flux'
+import CookieManager from 'react-native-cookies';
+
+// Change these to reflect
+const LOGIN_URL = "https://souka.io/accounts/login/";
+const HOME_URL = "https://souka.io/";
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  }
+});
+
+class AuthLogin extends Component {
+  onNavigationStateChange (navState) {
+    // If we get redirected back to the HOME_URL we know that we are logged in. If your backend does something different than this
+    // change this line.
+    if (navState.url == HOME_URL) {
+      this.setState({
+        loggedIn: true,
+      });
+      Actions.main({type: ActionConst.REPLACE});
+    }
+  }
+
+  render () {
+    // If we have completed loading the cookie choose to show Login WebView or the LoggedIn component, else just show an empty View.
+    return (
+      <View style={[styles.container]}>
+        <WebView
+          ref={'webview'}
+          automaticallyAdjustContentInsets={false}
+          style={styles.webView}
+          source={{uri: LOGIN_URL}}
+          javaScriptEnabled={true}
+          onNavigationStateChange={this.onNavigationStateChange.bind(this)}
+          startInLoadingState={true}
+          scalesPageToFit={true}
+        />
+      </View>
+    );
+  }
+}
+
+export default AuthLogin;
